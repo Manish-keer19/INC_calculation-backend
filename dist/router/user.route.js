3e6620e8b5b8b3453aa55a1202716d790654c627
manish <manishkeer530@gmail.com>
2025-10-24 16:21:40 +0530

    build the backend

--- dist/router/user.route.js
+++ dist/router/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { createUserData, getUserData } from '../controller/user.controller.js';
-import { authentication } from '../middleware/auth';
+import { authentication } from '../middleware/auth.js';
 export const userRouter = Router();
 userRouter.get("/get-data", authentication, getUserData);
 userRouter.post("/create-data", authentication, createUserData);
